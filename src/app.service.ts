d2b60804c6a1944b460f192a301b5432b11a4d87
Sergey Koksharov <gentoosiastic@gmail.com>
2023-05-10 22:27:37 +0300

    refactor: change hello message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Airways backend is up and running';
   }
 }
